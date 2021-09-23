76161bf52cee7feeaad47f873e03acbfa9128713
HamidAlisagar <you@example.com>
2021-09-23 16:44:13 +0500

    footer component added

--- src/components/navbar/UI/NavbarUi.js
+++ src/components/navbar/UI/NavbarUi.js
@@ -1,38 +1,49 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import  Styles from"../assets/css/NavbarUi.module.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import Styles from "../assets/css/NavbarUi.module.css";
 import logo from "../assets/icons/second-log.PNG";
 
 export default function NavbarUi() {
-    return (
-        <nav  className={`navbar navbar-expand-lg navbar-light bg-light py-3 ${Styles.header}`}>
-        <div  className={`container ${Styles.containerStyle}`}>
-          <Link className={`navbar-brand ${Styles.logo_style}`} to="/"><img src={logo}   alt="logo"/></Link> 
-          {/*
-          <button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span  className="navbar-toggler-icon"></span>
-          </button> */}
-          <div  className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul  className={`navbar-nav me-auto mb-2 mb-lg-0 ${Styles.list}`}>
-              <li   className={`nav-item ${Styles.liststyle} `}>
-                <Link to="/"  className={`nav-link ${Styles.linkstyle} `} aria-current="page">Home</Link>
-              </li>
-              <li  className={`nav-item  ${Styles.liststyle}`}>
-                <Link to="/Products"  className={`nav-link ${Styles.linkstyle} `} >Products</Link>
-              </li>
-              <li  className={`nav-item  ${Styles.liststylelast}`}>
-                <Link  className={`nav-link ${Styles.linkstyle} `}  to="/Login">Login</Link>
-              </li>
-              <li  className={`nav-item ${Styles.liststyle}`}>
-                <Link  className={`nav-link ${Styles.linkstyle} `} ><i className={`fas fa-cart-plus ${Styles.cart} `}> <span className={Styles.cartCounter}>12</span></i></Link>
-              </li>
-            </ul>
-
-          </div>
+  return (
+    <nav
+      className={`navbar navbar-expand-lg navbar-light bg-light py-3 ${Styles.header}`}
+    >
+      <div className={`container ${Styles.containerStyle}`}>
+        <Link className={`navbar-brand ${Styles.logo_style}`} to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className={`navbar-nav me-auto mb-2 mb-lg-0 ${Styles.list}`}>
+            <li className={`nav-item ${Styles.liststyle} `}>
+              <Link
+                to="/"
+                className={`nav-link ${Styles.linkstyle} `}
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </li>
+            <li className={`nav-item  ${Styles.liststyle}`}>
+              <Link to="/Products" className={`nav-link ${Styles.linkstyle} `}>
+                Products
+              </Link>
+            </li>
+            <li className={`nav-item  ${Styles.liststylelast}`}>
+              <Link className={`nav-link ${Styles.linkstyle} `} to="/Login">
+                Login
+              </Link>
+            </li>
+            <li className={`nav-item ${Styles.liststyle}`}>
+              <Link className={`nav-link ${Styles.linkstyle} `}>
+                <i className={`fas fa-cart-plus ${Styles.cart} `}>
+                  {" "}
+                  <span className={Styles.cartCounter}>12</span>
+                </i>
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-
-
-
-    )
+      </div>
+    </nav>
+  );
 }
--- src/pages/Home/UI/HomeUi.js
+++ src/pages/Home/UI/HomeUi.js
@@ -11,13 +11,14 @@ export default function HomeUi(props) {
           {props.products?.map((product,index) => {
             return (
               <div className="col-md-4 col-lg-4" key={index}>
-              <div className="card cardsize">
-                <img src={product.image} className="card-img-top sizeofimage" alt="..." />
+              <div className={`card  p-2 shadow ${Styles.cardStyel}`}>
+                <img src={product.image} className={`card-img-top ${Styles.sizeofimage}`} alt="..." />
                 <div className="card-body">
+                  <div className={Styles.cardBodyStyle}>
                  <div className="titlestyle"> <h5 className="card-title">{product.title}</h5> 
-                  <p className="card-text"> Price:<span className="text-danger">{product.price}$</span></p>
-                   {/* {/ <p className="card-text"> {product.description} </p> } */}
-                  <a href="#" className="btn btn-primary"> Add to Cart</a>
+                  <p className="card-text"> <b>Price:</b> &nbsp;<span className="text-danger">{product.price}$</span></p>
+                  <button className="btn btn-primary"> Add to Cart</button>
+                </div>
                 </div>
                 </div> 
               </div>
