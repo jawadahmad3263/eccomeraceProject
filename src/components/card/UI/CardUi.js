4509a69980d8e365454957ac0bfe4b53083799db
jawadahmad3263 <se.jawadahmad@gmail.com>
2021-09-24 11:52:15 +0500

    home and all product component updated

--- src/components/card/UI/CardUi.js
+++ src/components/card/UI/CardUi.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
-export default function CardUi() {
+import Styles from '../assets/css/CardUi.module.css'
+export default function CardUi(props) {
+    const {product,index} = props;
     return (
-        <div>
-            
+        <div className="col-md-4 col-lg-4" key={index}>
+        <div className={`card  p-2 shadow ${Styles.cardStyle}`}>
+          <img src={product.image} className={`card-img-top ${Styles.sizeofimage}`} alt="..." />
+          <div className="card-body">
+            <div className={Styles.cardBodyStyle}>
+           <div className="titlestyle"> <h5 className="card-title">{product.title}</h5> 
+            <p className="card-text"> <b>Price:</b> &nbsp;<span className="text-danger">{product.price}$</span></p>
+            <button className="btn btn-primary"> Add to Cart</button>
+          </div>
+          </div>
+          </div> 
+        </div>
         </div>
     )
 }
