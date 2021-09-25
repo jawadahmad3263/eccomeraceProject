3e6a11855c01ca25219087872867b1a92fdc7c6a
jawadahmad3263 <se.jawadahmad@gmail.com>
2021-09-25 22:37:03 +0500

    cart action cart reducer added and selected item is added to cart data

--- src/components/card/UI/CardUi.js
+++ src/components/card/UI/CardUi.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Styles from '../assets/css/CardUi.module.css'
 export default function CardUi(props) {
-    const {product,index} = props;
+    const {product,index,addItem,checkItem} = props;
     return (
         <div className="col-md-4 col-lg-4" key={index}>
         <div className={`card  p-2 shadow ${Styles.cardStyle}`}>
@@ -10,7 +10,9 @@ export default function CardUi(props) {
             <div className={Styles.cardBodyStyle}>
            <div className="titlestyle"> <h5 className="card-title">{product.title}</h5> 
             <p className="card-text"> <b>Price:</b> &nbsp;<span className="text-danger">{product.price}$</span></p>
-            <button className="btn btn-primary"> Add to Cart</button>
+            {checkItem===false?
+            <button className="btn btn-primary" onClick={addItem}> Add to Cart</button>:
+            <button className="btn btn-primary" onClick={addItem} disabled> item added</button>}
           </div>
           </div>
           </div> 
--- src/components/card/container/Card.js
+++ src/components/card/container/Card.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import CardUi from '../UI/CardUi'
-
+import { useDispatch,useSelector } from "react-redux";
+import cartAction from '../../../redux/actions/cartAction'
 export default function Card(props) {
+    var checkItem=false
+    const dispatch = useDispatch();
     const {product,index} = props;
+    //fun to add selected items to cart
+    const addItem = () => {
+        dispatch(cartAction.Add(product));
+    }
+    const addedItems= useSelector((state)=>state.cartReducer.cartData);
+    //check if item is added to cart before
+    checkItem=addedItems.includes(product);
+
     return (
-        <CardUi product={product} index={index} />
+        <CardUi product={product} index={index} addItem={addItem} checkItem={checkItem} />
     )
 }
