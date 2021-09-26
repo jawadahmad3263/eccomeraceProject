e3b13b511ad7e2dfc5748878997e89596861db85
HamidAlisagar <you@example.com>
2021-09-26 18:47:39 +0500

    all ui updated

--- src/components/totalprice/TotalPrice.js
+++ src/components/totalprice/TotalPrice.js
@@ -1,14 +1,15 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Styles from "./totalPrice.module.css";
 function TotalPrice() {
   return (
     <div className={`${Styles.mainPriceWrapper}`}>
-        <h5>The Total Amount of Items</h5>
-        
+      <h5> Total Number of Products</h5>
+
       <div className={Styles.priceContent}>
         <div className={Styles.totalPriceStyle}>
           <p>Total Price </p>
+
           <p>$300</p>
         </div>
         <div className={Styles.totalPriceStyle}>
@@ -17,15 +18,19 @@ function TotalPrice() {
         </div>
       </div>
       <div className={Styles.totalamount}>
-
-       <p><b className="h5">The Total Amount</b> <br/> including 
-       (shipping)</p>
-       <p>$310</p>
+        <p>
+          <b className="h5">The Total Amount</b> <br /> including (shipping)
+        </p>
+        <p>$310</p>
       </div>
       <div className={Styles.checkoutPageButton}>
-          <Link  to="/checkout" className={`btn btn-outline-primary text-uppercase ${Styles.btnStyle} `}>Checkout Page</Link>
+        <Link
+          to="/checkout"
+          className={`btn btn-outline-primary text-uppercase ${Styles.btnStyle} `}
+        >
+          Checkout Page
+        </Link>
       </div>
-     
     </div>
   );
 }
